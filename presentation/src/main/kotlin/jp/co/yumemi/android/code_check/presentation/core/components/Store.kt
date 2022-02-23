b6e2195f3cb835147d3c9fdbb08772f53cac9a16
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-24 03:22:23 +0900

    add: component classes

---
+++ presentation/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/core/components/Store.kt
@@ -0,0 +1,35 @@
+package jp.co.yumemi.android.code_check.presentation.core.components
+
+import jp.co.yumemi.android.code_check.presentation.core.contract.Event
+import jp.co.yumemi.android.code_check.presentation.core.contract.Intent
+import jp.co.yumemi.android.code_check.presentation.core.contract.State
+import jp.co.yumemi.android.code_check.presentation.core.contract.ViewState
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * The store exposes a stream of [State] and a stream of [Event] for the client to react to.
+ */
+interface Store<I : Intent, VS : ViewState, E : Event> {
+    /**
+     * Stream of [State] exposed to the client
+     */
+    val state: StateFlow<State<VS, E>>
+
+    /**
+     * Dispatches an [Intent]
+     */
+    fun dispatch(intent: I)
+
+    /**
+     * Cancels all jobs within the store
+     */
+    fun dispose()
+
+    /**
+     * Collect used for Kotlin Native
+     */
+    fun collect(
+        onState: ((State<VS, E>) -> Unit),
+    ): Job
+}
