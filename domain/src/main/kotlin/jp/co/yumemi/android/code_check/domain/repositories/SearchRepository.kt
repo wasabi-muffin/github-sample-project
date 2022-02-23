53da5578dc610a4c1e3c475e21ea58bedac1356f
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-23 17:03:04 +0900

    add: SearchRepository

--- domain/src/main/kotlin/jp/co/yumemi/android/code_check/domain/repositories/SearchRepository.kt
+++ domain/src/main/kotlin/jp/co/yumemi/android/code_check/domain/repositories/SearchRepository.kt
@@ -1,4 +1,7 @@
 package jp.co.yumemi.android.code_check.domain.repositories
 
+import jp.co.yumemi.android.code_check.domain.entities.GithubRepo
+
 interface SearchRepository {
+    fun searchRepos(searchText: String): List<GithubRepo>
 }
