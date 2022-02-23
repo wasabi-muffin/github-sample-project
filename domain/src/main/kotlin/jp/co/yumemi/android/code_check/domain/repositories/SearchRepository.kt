0cfa3ca6f12297688a7ad21816ac96ef33bac5d7
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-23 17:05:54 +0900

    add: suspend to repository

--- domain/src/main/kotlin/jp/co/yumemi/android/code_check/domain/repositories/SearchRepository.kt
+++ domain/src/main/kotlin/jp/co/yumemi/android/code_check/domain/repositories/SearchRepository.kt
@@ -3,5 +3,5 @@ package jp.co.yumemi.android.code_check.domain.repositories
 import jp.co.yumemi.android.code_check.domain.entities.GithubRepo
 
 interface SearchRepository {
-    fun searchRepos(searchText: String): List<GithubRepo>
+    suspend fun searchRepos(searchText: String): List<GithubRepo>
 }
