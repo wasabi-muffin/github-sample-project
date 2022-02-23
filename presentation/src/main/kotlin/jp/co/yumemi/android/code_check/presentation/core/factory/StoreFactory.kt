f182c091ea681e80c394e0e29b28a66f15d6a963
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-24 03:26:49 +0900

    add: store factory classes

---
+++ presentation/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/core/factory/StoreFactory.kt
@@ -0,0 +1,14 @@
+package jp.co.yumemi.android.code_check.presentation.core.factory
+
+import jp.co.yumemi.android.code_check.presentation.core.components.Store
+import jp.co.yumemi.android.code_check.presentation.core.contract.Action
+import jp.co.yumemi.android.code_check.presentation.core.contract.Event
+import jp.co.yumemi.android.code_check.presentation.core.contract.Intent
+import jp.co.yumemi.android.code_check.presentation.core.contract.Result
+import jp.co.yumemi.android.code_check.presentation.core.contract.State
+import jp.co.yumemi.android.code_check.presentation.core.contract.ViewState
+import jp.co.yumemi.android.code_check.presentation.core.middleware.Middleware
+
+interface StoreFactory<I : Intent, A : Action, R : Result, VS : ViewState, E : Event> {
+    fun create(initialState: State<VS, E>, middlewares: List<Middleware> = emptyList()): Store<I, VS, E>
+}
