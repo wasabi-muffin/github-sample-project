8d6de2c33e323dcd911278510eba146fb81d27c5
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-25 04:57:08 +0900

    fix: issues with state machine

--- presentation/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/feature/search/top/contract/SearchTopViewState.kt
+++ presentation/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/feature/search/top/contract/SearchTopViewState.kt
@@ -9,14 +9,16 @@ sealed class SearchTopViewState : ViewState {
 
     sealed class Stable : SearchTopViewState() {
         abstract val recentSearches: List<RecentSearch>
+        abstract val searchText: String
 
         @Parcelize data class EmptySearch(
+            override val searchText: String = "",
             override val recentSearches: List<RecentSearch>,
         ) : Stable()
 
         @Parcelize data class NonEmptySearch(
+            override val searchText: String,
             override val recentSearches: List<RecentSearch>,
-            val searchText: String,
         ) : Stable()
     }
 }
