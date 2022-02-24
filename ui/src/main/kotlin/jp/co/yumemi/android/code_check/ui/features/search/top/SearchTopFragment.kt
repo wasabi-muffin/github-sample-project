2bfd39a3a66e9126a3d74db64e2b86bb814c7447
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-24 23:34:13 +0900

    add: hilt annotations

--- ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/features/search/top/SearchTopFragment.kt
+++ ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/features/search/top/SearchTopFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.ui.core.compose
 
+@AndroidEntryPoint
 class SearchTopFragment : Fragment() {
+
+    private val viewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return requireContext().compose {
--- ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/features/search/top/SearchViewModel.kt
+++ ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/features/search/top/SearchViewModel.kt
@@ -1,5 +1,7 @@
 package jp.co.yumemi.android.code_check.ui.features.search.top
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class SearchViewModel : ViewModel()
