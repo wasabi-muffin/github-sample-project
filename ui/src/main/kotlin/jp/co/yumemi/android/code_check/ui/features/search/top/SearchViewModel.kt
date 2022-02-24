3d3340b6a3c8698f0f87e5987a9b30e92f60c3c6
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-24 23:49:20 +0900

    fix: hilt view model syntax

--- ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/features/search/top/SearchViewModel.kt
+++ ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/features/search/top/SearchViewModel.kt
@@ -2,6 +2,7 @@ package jp.co.yumemi.android.code_check.ui.features.search.top
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel : ViewModel()
+class SearchViewModel @Inject constructor() : ViewModel()
