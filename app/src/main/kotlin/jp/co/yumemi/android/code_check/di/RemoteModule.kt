bc23db06bb4947eb71acb829641e0c625e5207c8
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-27 05:55:57 +0900

    add: repo details di

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/di/RemoteModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/di/RemoteModule.kt
@@ -4,8 +4,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import jp.co.yumemi.android.code_check.data.sources.RepositoriesRemoteDataSource
 import jp.co.yumemi.android.code_check.data.sources.SearchRemoteDataSource
+import jp.co.yumemi.android.code_check.remote.apis.ReposApi
 import jp.co.yumemi.android.code_check.remote.apis.SearchApi
+import jp.co.yumemi.android.code_check.remote.providers.RepositoriesRemoteDataProvider
 import jp.co.yumemi.android.code_check.remote.providers.SearchRemoteDataProvider
 
 @Module
@@ -15,4 +18,9 @@ class RemoteModule {
     fun provideSearchRemoteDataSource(
         searchApi: SearchApi
     ): SearchRemoteDataSource = SearchRemoteDataProvider(searchApi)
+
+    @Provides
+    fun provideRepositoriesRemoteDataSource(
+        reposApi: ReposApi
+    ): RepositoriesRemoteDataSource = RepositoriesRemoteDataProvider(reposApi)
 }
