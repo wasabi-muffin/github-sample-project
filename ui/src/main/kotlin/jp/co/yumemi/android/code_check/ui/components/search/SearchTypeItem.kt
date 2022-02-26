a77abaa4f8d983607adb30f4071ee2c18fc91d2a
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-27 04:12:25 +0900

    refactor: search type ext

--- ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/components/search/SearchTypeItem.kt
+++ ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/components/search/SearchTypeItem.kt
@@ -1,17 +1,21 @@
 package jp.co.yumemi.android.code_check.ui.components.search
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,6 +26,8 @@ import jp.co.yumemi.android.code_check.domain.entities.SearchType
 import jp.co.yumemi.android.code_check.ui.R
 import jp.co.yumemi.android.code_check.ui.primitives.Github
 import jp.co.yumemi.android.code_check.ui.primitives.GithubTheme
+import jp.co.yumemi.android.code_check.ui.utils.icon
+import jp.co.yumemi.android.code_check.ui.utils.label
 
 @Composable
 fun SearchTypeItem(
@@ -32,21 +38,15 @@ fun SearchTypeItem(
     modifier = modifier.padding(24.dp),
     verticalAlignment = Alignment.CenterVertically
 ) {
-    val searchTypeIconId = when (searchType) {
-        SearchType.Repo -> R.drawable.repo
-    }
-    val searchTypeLabelId = when (searchType) {
-        SearchType.Repo -> R.string.common_repositories
-    }
     Icon(
-        painter = painterResource(id = searchTypeIconId),
+        painter = painterResource(id = searchType.icon()),
         contentDescription = "",
         tint = Github.Black,
         modifier = Modifier.size(24.dp)
     )
     Spacer(modifier = Modifier.width(16.dp))
     Text(
-        text = stringResource(id = R.string.search_top_search_with, stringResource(id = searchTypeLabelId), "\"$searchText\""),
+        text = stringResource(id = R.string.search_top_search_with, stringResource(id = searchType.label()), "\"$searchText\""),
         style = MaterialTheme.typography.h4,
         fontWeight = FontWeight.Normal,
         modifier = Modifier.weight(1F)
---
+++ ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/utils/SearchTypeExt.kt
@@ -0,0 +1,4 @@
+package jp.co.yumemi.android.code_check.ui.utils
+
+class SearchTypeExt {
+}
