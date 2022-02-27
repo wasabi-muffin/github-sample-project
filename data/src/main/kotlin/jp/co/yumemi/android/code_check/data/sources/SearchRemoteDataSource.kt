5f98397bae4cd6b178268ff45c53f2cc58c9c2eb
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-27 23:01:56 +0900

    add: count to search requests

--- data/src/main/kotlin/jp/co/yumemi/android/code_check/data/sources/SearchRemoteDataSource.kt
+++ data/src/main/kotlin/jp/co/yumemi/android/code_check/data/sources/SearchRemoteDataSource.kt
@@ -8,9 +8,9 @@ import jp.co.yumemi.android.code_check.data.models.SearchResultModel
 import jp.co.yumemi.android.code_check.data.models.UserModel
 
 interface SearchRemoteDataSource {
-    suspend fun searchRepositories(token: String? = null, searchText: String, pageNumber: Int = 0): SearchResultModel<RepositoryModel>
-    suspend fun searchIssues(token: String? = null, searchText: String, pageNumber: Int = 0): SearchResultModel<IssueModel>
-    suspend fun searchPullRequests(token: String? = null, searchText: String, pageNumber: Int = 0): SearchResultModel<PullRequestModel>
-    suspend fun searchUsers(token: String? = null, searchText: String, pageNumber: Int = 0): SearchResultModel<UserModel>
-    suspend fun searchOrganizations(token: String? = null, searchText: String, pageNumber: Int = 0): SearchResultModel<OrganizationModel>
+    suspend fun searchRepositories(token: String? = null, searchText: String, pageNumber: Int = 0, count: Int = 30): SearchResultModel<RepositoryModel>
+    suspend fun searchIssues(token: String? = null, searchText: String, pageNumber: Int = 0, count: Int = 30): SearchResultModel<IssueModel>
+    suspend fun searchPullRequests(token: String? = null, searchText: String, pageNumber: Int = 0, count: Int = 30): SearchResultModel<PullRequestModel>
+    suspend fun searchUsers(token: String? = null, searchText: String, pageNumber: Int = 0, count: Int = 30): SearchResultModel<UserModel>
+    suspend fun searchOrganizations(token: String? = null, searchText: String, pageNumber: Int = 0, count: Int = 30): SearchResultModel<OrganizationModel>
 }
