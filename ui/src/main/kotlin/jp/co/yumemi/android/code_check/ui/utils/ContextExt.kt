74cddb04549dabec8bc257ca2839a3af8da72557
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-28 13:50:57 +0900

    fix: remove require context

--- ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/utils/ContextExt.kt
+++ ui/src/main/kotlin/jp/co/yumemi/android/code_check/ui/utils/ContextExt.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import jp.co.yumemi.android.code_check.ui.primitives.GithubTheme
 
-fun Context.compose(content: @Composable () -> Unit): ComposeView = ComposeView(this).apply {
+fun Context.compose(content: @Composable Context.() -> Unit): ComposeView = ComposeView(this).apply {
     setContent {
         GithubTheme {
             content()
