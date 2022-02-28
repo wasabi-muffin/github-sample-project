88ca0dc43d347de30efabcca34430b972b620c0c
gmvalentino8 <gmvalentino8@gmail.com>
2022-02-28 22:11:39 +0900

    fix: add exception handler to tests

--- data/src/main/kotlin/jp/co/yumemi/android/code_check/data/error/DataException.kt
+++ data/src/main/kotlin/jp/co/yumemi/android/code_check/data/error/DataException.kt
@@ -5,8 +5,8 @@ sealed class DataException : Throwable() {
     abstract override val cause: Throwable?
 
     data class Unknown(
-        override val message: String?,
-        override val cause: Throwable?
+        override val message: String? = null,
+        override val cause: Throwable? = null,
     ) : DataException()
 
     /**
@@ -17,8 +17,8 @@ sealed class DataException : Throwable() {
          * Database
          */
         data class DatabaseException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
     }
 
@@ -30,56 +30,56 @@ sealed class DataException : Throwable() {
          * Timeout
          */
         data class NetworkException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
 
         /**
          * Http Error 401
          */
         data class UnauthorizedException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
 
         /**
          * Http Error 403
          */
         data class ForbiddenException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
 
         /**
          * Http Error 404
          */
         data class NotFoundException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
 
         /**
          * Http Error 422
          */
         data class UnprocessableException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
 
         /**
          * Http Error 500
          */
         data class InternalServerException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
 
         /**
          * Http Error 503
          */
         data class ServiceUnavailableException(
-            override val message: String?,
-            override val cause: Throwable?
+            override val message: String? = null,
+            override val cause: Throwable? = null,
         ) : RemoteException()
     }
 }
