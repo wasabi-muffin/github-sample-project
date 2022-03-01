37766d66868c31c5a6cabb1fc4094b1e03629f36
gmvalentino8 <gmvalentino8@gmail.com>
2022-03-02 06:09:10 +0900

    refactor: move run handling to error handler

--- domain/src/main/kotlin/jp/co/yumemi/android/code_check/domain/core/DomainError.kt
+++ domain/src/main/kotlin/jp/co/yumemi/android/code_check/domain/core/DomainError.kt
@@ -3,7 +3,7 @@ package jp.co.yumemi.android.code_check.domain.core
 /**
  * Domain error
  *
- * Wrapper for throwables
+ * Wrapper for [Throwable]s
  *
  * [Exception]s should be caught in the domain layer and passed as [DomainError]s to the presentation layer
  */
@@ -14,17 +14,4 @@ sealed class DomainError(
     data class Server(override val throwable: Throwable) : DomainError(throwable)
     data class Maintenance(override val throwable: Throwable) : DomainError(throwable)
     data class Unknown(override val throwable: Throwable) : DomainError(throwable)
-}
-
-/**
- * Run handling
- *
- * Extension to run a [block] and map exceptions using an [ErrorHandler] on failure
- */
-inline fun <T, R> T.runHandling(handler: ErrorHandler, block: T.() -> R): DomainResult<R> {
-    return try {
-        DomainResult.Success(block())
-    } catch (e: Throwable) {
-        DomainResult.Failure(handler.handle(e))
-    }
 }
