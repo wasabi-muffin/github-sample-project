f44d865f6d362130f767d25470d9f0ecea3fac7a
gmvalentino8 <gmvalentino8@gmail.com>
2022-03-02 04:43:19 +0900

    fix: rename prod to prd

---
+++ app/src/prd/kotlin/jp/co/yumemi/android/code_check/di/DataModule.kt
@@ -0,0 +1,28 @@
+package jp.co.yumemi.android.code_check.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import jp.co.yumemi.android.code_check.data.repositories.RepositoriesDataRepository
+import jp.co.yumemi.android.code_check.data.repositories.SearchDataRepository
+import jp.co.yumemi.android.code_check.data.sources.RepositoriesRemoteDataSource
+import jp.co.yumemi.android.code_check.data.sources.SearchLocalDataSource
+import jp.co.yumemi.android.code_check.data.sources.SearchRemoteDataSource
+import jp.co.yumemi.android.code_check.domain.repositories.RepositoriesRepository
+import jp.co.yumemi.android.code_check.domain.repositories.SearchRepository
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+class DataModule {
+    @Provides
+    fun provideSearchRepository(
+        searchRemoteDataSource: SearchRemoteDataSource,
+        searchLocalDataSource: SearchLocalDataSource,
+    ): SearchRepository = SearchDataRepository(searchRemoteDataSource, searchLocalDataSource)
+
+    @Provides
+    fun provideRepositoriesRepository(
+        repositoriesRemoteDataSource: RepositoriesRemoteDataSource
+    ): RepositoriesRepository = RepositoriesDataRepository(repositoriesRemoteDataSource)
+}
