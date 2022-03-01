f892ebf1d8b5512579a3f80e5f563f5b6aeb31a2
gmvalentino8 <gmvalentino8@gmail.com>
2022-03-02 00:34:08 +0900

    add: refactor packages

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/app/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/app/MainActivity.kt
@@ -1,10 +1,11 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check
+package jp.co.yumemi.android.code_check.app
 
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.android.code_check.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main)
